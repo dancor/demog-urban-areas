1c1be615a5616412ae3c1f669fe3e1cabfddf043
Daniel Corson <danl@alum.mit.edu>
2012-11-15 17:19:42 -0500

    mapping to UN countries (e.g. Greenland associated with Denmark)

--- src/Main.hs
+++ src/Main.hs
@@ -50,15 +50,15 @@ lineGetType cs =
     ]
 
 summ :: String -> String
-summ s = 
-  if length s <= half * 2 
-    then s 
+summ s =
+  if length s <= half * 2
+    then s
     else take (half - 1) s ++ ".." ++ drop (length s - half + 1) s
   where
   half = 80
 
-toChunk lineType cs = 
-  if null rem then Nothing else Just (snd $ head rem, tail rem) 
+toChunk lineType cs =
+  if null rem then Nothing else Just (snd $ head rem, tail rem)
   where
   rem = dropWhile ((/= lineType) . fst) cs
 
@@ -79,17 +79,31 @@ getPage cs = do
   (cc, cs2) <- toChunk Word cs
   let (countries:cities:_) = chunksOf (length cc `div` 2) cc
   (lol, cs3) <- toChunk Num cs2
-  let [pops, yrs, pop2s, area2s, dens2s, areas, denss, yr2s] = 
+  let [pops, yrs, pop2s, area2s, dens2s, areas, denss, yr2s] =
         chunksOf (length lol `div` 8) lol
       r = case getPage cs3 of
         Nothing -> []
         Just r2 -> r2
   return (zip3 countries cities pops ++ r)
 
-nationIsUN n =
-  not $ any (`isInfixOf` n) [
-    ":", "Germany-", "France-", "Switzerland &", "Austria &", "Bermuda",
-    "Greenland", "Western Sahara"]
+unIfy :: String -> String
+unIfy "Austria & Germany" = "Austria"
+unIfy "Bermuda" = "UK"
+unIfy "China: Taiwan" = "China"
+unIfy "France-Belgium" = "France"
+unIfy "France: Mayotte" = "France"
+unIfy "Germany-France" = "Germany"
+unIfy "Germany-Netherlands" = "Germany"
+unIfy "Greenland" = "Denmark"
+unIfy "Switzerland & D & F" = "Switzerland"
+unIfy "Switzerland & F" = "Switzerland"
+unIfy "US: American Samoa" = "USA"
+unIfy "US: Guam" = "USA"
+unIfy "US: N. Marianas" = "USA"
+unIfy "US: Puerto Rico" = "USA"
+unIfy "US:Virgin Islands" = "USA"
+unIfy "Western Sahara" = "Morocco"
+unIfy x = x
 
 -- I have a certain set of names and abbrs. that I tend to stick too.
 -- Also some other normalization and cleanup.
@@ -118,7 +132,7 @@ n2cp :: (String, String, String) -> (String, (Int, String))
 n2cp (n, c, p) = (n, (read $ filter isDigit p, c))
 
 expNot :: Double -> (Double, Int)
-expNot x = 
+expNot x =
   (c, e)
   where
   e = floor (logBase 10 x)
@@ -128,15 +142,15 @@ expNot x =
 expNotSigDigs :: Int -> Double -> (Int, Int)
 expNotSigDigs d x = (cSig', e')
   where
-  (cSig', e') = 
+  (cSig', e') =
     if length (show cSig) > d
-      then (cSig `div` 10, e + 1) 
+      then (cSig `div` 10, e + 1)
       else (cSig, e)
   cSig = round $ c * (10 ** fromIntegral (d - 1))
   (c, e) = expNot x
 
 placeDecimal :: Int -> Int -> String
-placeDecimal pos x = 
+placeDecimal pos x =
   l ++ (if null r then "" else "." ++ r)
   where
   l = take pos (sx ++ repeat '0')
@@ -144,7 +158,7 @@ placeDecimal pos x =
   sx = show x
 
 metricSigDigs :: Int -> Double -> (String, String)
-metricSigDigs d x = 
+metricSigDigs d x =
   if e >= 6
     then (placeDecimal (e + 1 - 6) c, "M")
     else
@@ -154,13 +168,13 @@ metricSigDigs d x =
   where
   (c, e) = expNotSigDigs d x
   ds = show c
-    
+
 showN :: Int -> String
-showN x = 
+showN x =
   c ++ suf
   where
-  (c, suf) = metricSigDigs 2 (fromIntegral x) 
-  
+  (c, suf) = metricSigDigs 2 (fromIntegral x)
+
 showLol :: (Maybe String, (Int, String)) -> String
 showLol (n, (p, c)) = showN p ++ " " ++ (maybe "" (++ ": ") n) ++ c
 
@@ -171,7 +185,7 @@ main = do
     let typeChunks =
             map (\ xs -> (fst (head xs), map snd xs)) .
             groupBy ((==) `on` fst) $
-            map (\ l -> (lineGetType l, l)) ls    
+            map (\ l -> (lineGetType l, l)) ls
         usageErr = error "Program was invoked with invalid arguments."
         runTypeArg = case args of
             [] -> "all"
@@ -180,27 +194,34 @@ main = do
         onePerNation = nubBy ((==) `on` fst)
         showNation = map (first Just)
         hideNation = map (first (const Nothing))
+        unIfyNations = map (first unIfy)
         (runType, filterFunc, finalFunc) = case runTypeArg of
-            "all" -> (runTypeArg, const True, map showLol . showNation)
+            "all" ->
+                ( runTypeArg
+                , const True
+                , map showLol . showNation)
             "nation_by_count" ->
                 ( runTypeArg
-                , \ (nation, (population, city)) ->
-                    nationIsUN nation && population >= 500000
+                , \ (_nation, (population, _city)) -> population >= 500000
                 , map (\ (count, nation) -> show count ++ " " ++ nation) .
                   (\ xs -> xs ++ [(sum (map fst xs), "total")]) .
                   sortBy (flip (comparing fst) `mappend` comparing snd) .
-                  map (\ x -> (length x, fst $ head x)) . 
-                  groupBy ((==) `on` fst) . sort
+                  map (\ x -> (length x, fst $ head x)) .
+                  groupBy ((==) `on` fst) . sort . unIfyNations
                 )
-            "un1" -> 
+            "un1" ->
                 ( runTypeArg
-                , nationIsUN . fst
-                , map showLol . showNation . onePerNation
+                , const True
+                , map showLol . showNation . onePerNation . unIfyNations
                 )
-            n -> ("by_nation" </> n, (== n) . fst, map showLol . hideNation)
+            n ->
+                ( "by_nation" </> n
+                , (== n) . unIfy . fst
+                , map showLol . hideNation
+                )
     {- analysis phase:
     putStr $ unlines $ map summ $
-      map (\ (a, b) -> [head $ show a] ++ show (length b) ++ " " ++ 
+      map (\ (a, b) -> [head $ show a] ++ show (length b) ++ " " ++
                           intercalate " " b)
       typeChunks
 -- w(country) w(city) n(pop) n(yr) n(pop2,area2) n(dens2) n(area) n(dens,yr2)
